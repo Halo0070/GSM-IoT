4b8c98746ef171da88e636971ff7d408a09ce995
Halo0070 <lee998598@gmail.com>
2019-05-12 18:02:14 +0900

    Update www.js

--- IoTServer/www.js
+++ IoTServer/www.js
@@ -22,3 +22,28 @@ mongoDB.connect(url, function(err, db) {
     dbObj=db;
     console.log("DB Connect....");
 });
+
+// Listen on provided port, on all network interfaces.
+
+var mqtt=require("mqtt");	// A a=new A();
+var client=mqtt.connect("mqtt://192.168.0.15");
+client.on("connect", function(){
+	console.log("mqtt server connection...");
+	client.subscribe("iot");
+});
+
+client.on("message", function(topic, message){
+	if(topic=='iot'){
+		console.log(message.toString());
+		//db insert
+		var obj=JSON.parse(message);	//txt->object
+		obj.created_at=new Date();	// hum, tmp, created_at
+		console.log(obj);
+		var dht11=dbObj.collection("dht11");	//DB table 만들기
+		dht11.save(obj, function(err, result){
+			console.log(JSON.stringify(result));	//{ok : 1}
+		});
+	}
+});
+
+//--MongoDB--->Node.js--->HTML(javascript)|<---IE,Android(모니터링)
