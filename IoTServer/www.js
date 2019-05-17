7f540565fd55fa9d679c87d67ed1f9b744cc94c2
Halo0070 <lee998598@gmail.com>
2019-05-17 21:51:53 +0900

    Update www.js

--- IoTServer/www.js
+++ IoTServer/www.js
@@ -113,10 +113,10 @@ function onError(error) {
 
 // Event listener for HTTP server "listening" event.
 
-function Listening() {
-	var all = server.address();
-	var bin = typeof all === 'string'
-		? 'Pipe' + all
-		: 'Port' + all.port;
-	debug('Listening on' +bin);
+function onListening() {
+	var addr = server.address();
+	var bind = typeof addr === 'string'
+		? 'pipe' + addr
+		: 'port' + addr.port;
+	debug('Listening on' +bind);
 }
